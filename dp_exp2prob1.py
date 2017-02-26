96df2809cd1cbeaf0b06766cea98b7653e6e3012
Prasad Mate <prasad_mate@hotmail.com>
2017-02-26 15:07:34 -0500

    Add files via upload
    (Differential) Data Privacy Asssignment

---
+++ dp_exp2prob1.py
@@ -0,0 +1,166 @@
+import numpy as np;
+import pandas as pd;
+import matplotlib.pyplot as plt;
+
+#global variables
+data = []
+P_data={} #probability distribution of original data
+S_data={} #probability distribution of synthetic data
+
+#helper functions
+
+def build_histogram():
+
+    #compute distribution P and return
+    counts = data.TL_BR.value_counts().values
+    bins = data.TL_BR.value_counts().index._data
+    total = sum(counts)
+    distribution = dict(zip(list(bins),list(counts)))
+    # ------------------------------------------------
+    plot_histogram(distribution,num=1,title='Histogram of Original DataSet')
+    #--------------------------------------------------
+    for key,value in distribution.items():
+        distribution[key]=float(value/total)
+    return distribution
+
+
+def build_noisy_histogram(sensitivity=1,epsilon=0.1):
+    list_of_counts = data.TL_BR.value_counts().values
+    bins = data.TL_BR.value_counts().index._data
+    for i in range(len(list_of_counts)):
+        list_of_counts[i] = list_of_counts[i] + _laplaceNoise_(sensitivity,epsilon)
+        if list_of_counts[i] < 0:
+            list_of_counts[i] = 0
+
+    bin_count_map = dict(zip(bins, list_of_counts))
+    #plot_histogram(bin_count_map)
+
+    return bin_count_map
+
+def plot_histogram(bin_to_count_map,num = 1,title = 'Histogram'):
+    print("################ Plotting ", title ," #########################")
+    bins = list(bin_to_count_map.keys())
+    dummyX = np.arange(1, len(bins) + 1, 1)
+    freq = list(bin_to_count_map.values())
+    plt.figure(num = num,facecolor='gray')
+    plt.bar(dummyX, freq)
+    plt.xticks(dummyX, bins, rotation='horizontal')
+    plt.xlabel(" BINS (TL + BR) ")
+    plt.ylabel(" Frequency ")
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
+
+def plot_lineGraph(epsilons,distance):
+    print('PLOTTING DISTANCE VS PRIVACY BUDGET')
+    plt.figure(num=3,facecolor='green')
+    plt.plot(epsilons,distance)
+    plt.xlabel(" Privacy Budget ")
+    plt.ylabel(" DTV ")
+    plt.title(r'$\mathrm{Histogram\ of\ Distribution Variance  }$')
+    plt.grid(True)
+    plt.show()
+    pass
+
+def build_normalized_histogram(bins_to_count_map):
+    totalFrq = sum(bins_to_count_map.values())
+    for key,value in bins_to_count_map.items():
+        bins_to_count_map[key] = value/totalFrq;
+
+    plot_histogram(bins_to_count_map,num=3,title='Histogram of Normalized Sanitized Data')
+    return bins_to_count_map
+
+def _laplaceNoise_(sensitivity,epsilon,return_size=1):
+    if(epsilon > 0):
+        y = sensitivity / epsilon
+    else:
+        y = 10
+
+    noise = np.random.laplace(loc=0, scale=y, size = return_size )
+    return noise
+
+
+def build_sanitized_database(bin_map_count_dist):
+    bin_index = list(bin_map_count_dist.keys());
+    sanitized_bin_count = (np.random.multinomial(958, list(bin_map_count_dist.values()), size=1)).tolist()
+    sanitized_bin_count_map = dict(zip(bin_index,sanitized_bin_count[0]))
+    return sanitized_bin_count_map
+
+def computeSanitizedDistribution(epsilon = 0.1):
+    ep = epsilon
+    bin_map_count_dist = build_noisy_histogram(sensitivity=1, epsilon=ep)  # returns noisy bin counts
+    total = sum(bin_map_count_dist.values())
+
+    for key, value in bin_map_count_dist.items():
+        # calculate probability disturibution for each bin
+        S_data[key] = (value / total)
+
+    return S_data
+
+def computeTotalVariationDistance():
+    global P_data
+    distance =[]
+    epsilons = np.arange(0.1,1.1,0.1)
+#do not generate new noise each time
+    for ep in epsilons:
+        for it in range(40):
+            S = 0
+            s_avg = 0
+            S_data = computeSanitizedDistribution(epsilon=ep)
+            for k in P_data.keys():
+                S = S + abs(P_data[k] - S_data[k])
+            S = 0.5*S
+            s_avg = s_avg + S
+        distance.append(float(s_avg/40))
+    plot_lineGraph(epsilons,distance)
+
+
+
+def readFileData(file):
+    data = pd.read_csv(file)
+    return data;
+
+#main functions
+
+"""
+    The program control begins here.
+"""
+def runExp2():
+   global data,P_data
+   S_data={}
+
+   print('----------------------  Reading TIC TAC TOE DATA ---------------')
+   data = readFileData('tic-tac-toe.txt');
+
+#--------------PREPARE DATA-------------------------
+#Make additional column TL_BR by concatenation TL and BR column values
+   newCol = []
+   for i in range(len(data)):
+       newCol.append(data.TL.values[i] + data.BR.values[i])
+   se = pd.Series(newCol)
+   data['TL_BR'] = se.values
+
+   data.to_csv('temp.txt')
+#----------------------------------------------------------
+
+   # plot histogram of TL and BR squares of tic tac toe game
+   P_data = build_histogram() #returns distribution of original data say 'P"
+
+   bin_map_count_dist = build_noisy_histogram(sensitivity=1,epsilon=0.1) #returns noisy bin counts
+    #------------Normalizing bin count------------------
+   total = sum(bin_map_count_dist.values())
+
+   for key,value in bin_map_count_dist.items():
+       #calculate probability disturibution for each bin
+        S_data[key] = (value/total)
+
+   sanitized_bin_count_map = build_sanitized_database(S_data); # builds sanitized database using multinomial sampling from noisy database and returns sanitized bin_counts
+   #-----------------------------------------------
+   plot_histogram(sanitized_bin_count_map,num=2,title='Histogram of Sanitized Data')
+   build_normalized_histogram(sanitized_bin_count_map)
+
+   computeTotalVariationDistance()
+
+#initialize procedure calls
+runExp2();
+
---
+++ dp_exp2prob2.py
@@ -0,0 +1,161 @@
+import numpy as np;
+import pandas as pd;
+import matplotlib.pyplot as plt;
+import timeit
+
+#global variables
+
+data = pd.read_csv('mushroomDB.txt')
+
+#helper functions
+def binarize_dataSet():
+    # To read data and binarized
+
+    # First binarize edibility column values
+    new_binary_values_for_edibility = []
+    for value in data['edibility']:
+        if value == 'p':
+            new_binary_values_for_edibility.append(int(1))
+        else:
+            new_binary_values_for_edibility.append(int(-1))
+    data['edibility' + '0/1'] = pd.Series(new_binary_values_for_edibility).values
+
+    for i in range(1, 23):
+        if (i == 11):  # skip column number 11
+            continue
+        attr_splits = data.ix[:, i].unique()
+        attr_name = data.ix[:, i].name
+
+        for col_value in attr_splits:
+            new_binary_attr = attr_name + '-' + col_value
+
+            new_binary_values = []
+            for value in data[data.ix[:, i].name]:
+                if value == col_value:
+                    new_binary_values.append(int(1))
+                else:
+                    new_binary_values.append(int(-1))
+            data[new_binary_attr] = pd.Series(new_binary_values).values
+
+    data.to_csv('binarizedDataSet.txt')
+
+def logit(x):
+    try:
+        prob = 1/ (1 + np.exp(x))
+    except Exception:
+        print(Exception)
+    return prob
+
+#calculate optimum value of Beta using gradient descent
+def gradient_descent(beta,alpha,num_of_iter):
+    initial_likelhood_value = compute_log_likelihood(beta)
+    likelihood_cost.append(initial_likelhood_value)
+    for i in range(num_of_iter):
+        gradient = compute_gradient(beta)
+        beta = beta - alpha * gradient
+        likelihood_value = compute_log_likelihood(beta)
+        likelihood_cost.append(likelihood_value)
+
+    return beta,likelihood_cost
+
+def compute_gradient(beta):
+    # using gradient descent formula
+    grad = np.zeros(m)
+    for t in range(n - 1):
+        sig = logit(Y.T[t:t + 1] * np.dot(beta, X[t, :]))
+        row_vect_response = Y.T[t:t + 1] * X[t, :]
+
+        grad = grad + (1 - sig ) * row_vect_response
+    return grad
+
+def compute_log_likelihood(beta):
+    log_cost = 0
+    for t in range(n):
+        row_response_vect = Y.T[t:t + 1] * np.dot(beta,X[t,:]) #beta into t th row in training data
+        log_cost = log_cost + np.log(logit(row_response_vect))
+    return log_cost
+
+def model_accuracy(dataset,response,model):
+
+    resp = np.dot(dataset,model)
+
+    for i in range(len(resp)):
+        if logit(resp[i]) > 0.5:
+            resp[i] = 1
+        else:
+            resp[i] = -1
+    print(resp)
+    H = np.ones(response.shape[0]) + (np.array(resp) * np.array(response))
+
+    acc = sum(np.array(H)) * 100 /(2*H.shape[0])
+    return acc
+
+def plot_likelihood_VS_iterations(likelihood_cost):
+    print('======================  Plotting likelihood VS number of iterations ===================================')
+    plt.figure(num = 1)
+    plt.plot(list(range(0,num_of_iter+1)),likelihood_cost)
+    plt.xlabel('No of iterations')
+    plt.ylabel('Likelihood Cost')
+    plt.title(' Likelihood graph')
+    plt.grid(True)
+    plt.show()
+
+#initialize function calls
+
+print('##########  Preparing Data  ###########')
+cond = True
+while (cond):
+    print("Binarize dataset :: Yes - 0 \n No - 1")
+    index = int(input("Enter the choice"))
+    if index == 0:
+        binarize_dataSet()
+    elif index != 0 and index == 1:
+        cond = False
+    else: cond=True
+
+
+# Data set for learning classification
+
+binData = pd.read_csv('binarizedDataSet.txt')
+
+data_size = binData.shape[0]
+training_data_size = 7000
+test_data_size = data_size - training_data_size
+
+A = binData.loc[0:training_data_size - 1].as_matrix()  # read 7000 entries for training
+B = binData.loc[training_data_size:data_size-1].as_matrix()  #read 1124 entries for testing
+X = np.delete(A,np.s_[0:25],1)   #delete the redandant columns from training data
+X_test = np.delete(B,np.s_[0:25],1)  #delete the redandant columns from test data
+X = np.c_[np.ones(training_data_size),X]  #add ones columns in training data
+X_test = np.c_[np.ones(test_data_size),X_test] #add ones column in test data
+#--------------Save training and test data in CSV files-----------
+np.savetxt("train.csv", X, delimiter=",")
+np.savetxt("test.csv",X_test,delimiter=",")
+#----------------------------------------------------------------
+Y = binData['edibility0/1'].loc[0:training_data_size - 1].as_matrix().T #initialize response vector Y
+Y_test = binData['edibility0/1'].loc[training_data_size:data_size].as_matrix().T
+#defining parameters
+n = X.shape[0] #no of rows of training data
+m = X.shape[1] #no of features
+beta = np.zeros(m) # initialize beta vector
+alpha = 0.01 # initialize learning rate
+num_of_iter = 100
+likelihood_cost = [] #log likelihood function
+print('$$$$$$$$$$$$$$  TRAINING LOGISTIC REGRESSION MODEL  -   MUSHROOM DATA  $$$$$$$$$$$$$$$$$$$$$$ \n')
+start = timeit.default_timer()
+#gradient descent
+[beta,likelihood_cost] = gradient_descent(beta,alpha,num_of_iter)
+stop = timeit.default_timer()
+print ('Run time for training :: ',stop - start)
+print('############   BeTa   AfTeR ', num_of_iter ,'iTERatIONS  ###############')
+print(beta)
+print('\n $$$$$$$$$$$$$$$$ Likelihood Cost  $$$$$$$$$$$$$$$$$$')
+print(likelihood_cost)
+
+#graphical interpretations
+
+plot_likelihood_VS_iterations(likelihood_cost)
+#Training Data Accuracy
+print('Training accuracy achieved :: ',model_accuracy(X,Y,beta) , ' %')
+#Test Data Accurancy
+print('Testing accuracy achieved :: ',model_accuracy(X_test,Y_test,beta) , ' %')
